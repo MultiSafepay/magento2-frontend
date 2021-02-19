a139b806fe096106016955e9d5c12f9854a2c63a
Vinod Sowdagar <53818758+Vinod-MultiSafepay@users.noreply.github.com>
2021-02-19 17:58:25 +0100

    Feature/generic gateway (#84)
    * Add generic gateway
    
    * Add support for gateway image
    
    * Support image and non image gateway

---
+++ view/frontend/web/js/view/payment/gateway/method-renderer/genericgateway.js
@@ -0,0 +1,99 @@
+/**
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is provided with Magento in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * Copyright © 2021 MultiSafepay, Inc. All rights reserved.
+ * See DISCLAIMER.md for disclaimer details.
+ *
+ */
+
+/*browser:true*/
+/*global define*/
+define(
+    [
+        'jquery',
+        'Magento_Checkout/js/view/payment/default',
+        'Magento_Checkout/js/checkout-data',
+        'Magento_Checkout/js/action/redirect-on-success',
+        'mage/url'
+    ],
+
+    /**
+     *
+     * @param $
+     * @param Component
+     * @param checkoutData
+     * @param redirectOnSuccessAction
+     * @param url
+     * @returns {*}
+     */
+    function (
+        $,
+        Component,
+        checkoutData,
+        redirectOnSuccessAction,
+        url
+    ) {
+        const config = window.checkoutConfig.payment.multisafepay_genericgateway;
+        'use strict';
+
+        return Component.extend({
+            defaults: {
+                template: 'MultiSafepay_ConnectFrontend/payment/gateway/genericgateway',
+                transactionResult: ''
+            },
+
+            initObservable: function () {
+                this._super();
+
+                if (!checkoutData.getSelectedPaymentMethod() && config.is_preselected) {
+                    this.selectPaymentMethod();
+                }
+
+                return this;
+            },
+
+            /**
+             * Get the gateway code
+             *
+             * @returns {string}
+             */
+            getCode: function () {
+                return 'multisafepay_genericgateway';
+            },
+
+            /**
+             * Get the gateway image
+             *
+             * @returns {string}
+             */
+            getImage: function () {
+                return config.image;
+            },
+
+
+            /**
+             * Returns if the image is available
+             *
+             * @returns {boolean}
+             */
+            isImageAvailable: function () {
+                return !!config.image;
+
+            },
+
+            /**
+             * Redirect to controller after place order
+             */
+            afterPlaceOrder: function () {
+                redirectOnSuccessAction.redirectUrl = url.build('multisafepay/connect/redirect/');
+                this.redirectAfterPlaceOrder = true;
+            }
+        });
+    }
+);
