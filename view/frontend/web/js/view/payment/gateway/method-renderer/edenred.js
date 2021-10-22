e0c97e7d64d816343779da44b57e02841993d19b
Stanislau Kharlap <stanislau.kharlap@multisafepay.com>
2021-10-22 16:33:41 +0200

    added coupons to transaction request data

---
+++ view/frontend/web/js/view/payment/gateway/method-renderer/edenred.js
@@ -0,0 +1,61 @@
+/**
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is provided with Magento in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * Copyright © 2021 MultiSafepay, Inc. All rights reserved.
+ * See DISCLAIMER.md for disclaimer details.
+ *
+ */
+
+/*browser:true*/
+/*global define*/
+define(
+    [
+        'jquery',
+        'MultiSafepay_ConnectFrontend/js/view/payment/method-renderer/base-renderer',
+        'Magento_Checkout/js/checkout-data',
+        'Magento_Checkout/js/action/redirect-on-success',
+        'mage/url'
+    ],
+
+    /**
+     *
+     * @param $
+     * @param Component
+     * @param checkoutData
+     * @param redirectOnSuccessAction
+     * @param url
+     * @returns {*}
+     */
+    function (
+        $,
+        Component,
+        checkoutData,
+        redirectOnSuccessAction,
+        url
+    ) {
+        'use strict';
+
+        return Component.extend({
+            defaults: {
+                template: 'MultiSafepay_ConnectFrontend/payment/generic',
+            },
+
+            /**
+             * Get payment method type.
+             */
+            getTitle: function () {
+                if (!$.isEmptyObject(this.paymentConfig.coupons)) {
+                    return this.item.title + ' (' +  this.paymentConfig.coupons.join(', ').toUpperCase() + ')';
+                }
+
+                return this.item.title;
+            },
+        });
+    }
+);
