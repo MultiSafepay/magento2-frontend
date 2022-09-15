f3004c8c0a8236199b5ce625b516eb103d756ce7
Daniel Civit <daniel.civit@multisafepay.com>
2022-09-15 10:25:11 +0200

    PLGMAG2V2-553: Fix integration tests (#204)
    * PLGMAG2V2-553: Fix integration tests

--- .github/workflows/magento-integration-tests-pre-phpunit.sh
+++ .github/workflows/magento-integration-tests-pre-phpunit.sh
@@ -6,9 +6,32 @@ if [[ $GITHUB_REPOSITORY == *"internal"* ]] ; then
     REPO_SUFFIX="-internal"
 fi
 
-composer config repositories.multisafepay-php-sdk vcs git@github.com:MultiSafepay/php-sdk${REPO_SUFFIX}.git
-composer config repositories.multisafepay-core vcs git@github.com:MultiSafepay/magento2${REPO_SUFFIX}-core.git
-composer config repositories.multisafepay-adminhtml vcs git@github.com:MultiSafepay/magento2${REPO_SUFFIX}-adminhtml.git
+# PHP-SDK
+if [[ $(curl -s -u ${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN https://api.github.com/repos/MultiSafepay/php-sdk${REPO_SUFFIX}/branches | grep -iGc '"name": "'${CURRENT_HEAD_REF}'"') == 1 ]]; then
+    PHP_SDK_BRANCH_NAME=${CURRENT_HEAD_REF}
+else
+    PHP_SDK_BRANCH_NAME="master"
+fi
+git clone -b ${PHP_SDK_BRANCH_NAME} --single-branch https://${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN@github.com/MultiSafepay/php-sdk${REPO_SUFFIX}.git ./package-source/multisafepay/php-sdk/
 
+
+# MAGENTO 2 CORE MODULE
+if [[ $(curl -s -u ${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN https://api.github.com/repos/MultiSafepay/magento2${REPO_SUFFIX}-core/branches | grep -iGc '"name": "'${CURRENT_HEAD_REF}'"') == 1 ]]; then
+    CORE_MODULE_BRANCH_NAME=${CURRENT_HEAD_REF}
+else
+    CORE_MODULE_BRANCH_NAME="master"
+fi
+git clone -b ${CORE_MODULE_BRANCH_NAME} --single-branch https://${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN@github.com/MultiSafepay/magento2${REPO_SUFFIX}-core.git ./package-source/multisafepay/magento2-core/
+
+# MAGENTO 2 ADMIN MODULE
+if [[ $(curl -s -u ${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN https://api.github.com/repos/MultiSafepay/magento2${REPO_SUFFIX}-adminhtml/branches | grep -iGc '"name": "'${CURRENT_HEAD_REF}'"') == 1 ]]; then
+    ADMIN_MODULE_BRANCH_NAME=${CURRENT_HEAD_REF}
+else
+    ADMIN_MODULE_BRANCH_NAME="master"
+fi
+git clone -b ${ADMIN_MODULE_BRANCH_NAME} --single-branch https://${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN@github.com/MultiSafepay/magento2${REPO_SUFFIX}-adminhtml.git ./package-source/multisafepay/magento2-adminhtml/
+
+
+composer config repositories.multisafepay "path" "package-source/multisafepay/*"
 composer config minimum-stability dev
 composer config prefer-stable false
