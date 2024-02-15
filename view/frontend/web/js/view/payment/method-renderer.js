75efe0b99046926712efd26131f186e7eb459662
Vinod Sowdagar <53818758+Vinod-MultiSafepay@users.noreply.github.com>
2024-02-15 18:49:00 +0100

    DAVAMS-724: Add MB WAY (#263)

--- view/frontend/web/js/view/payment/method-renderer.js
+++ view/frontend/web/js/view/payment/method-renderer.js
@@ -74,6 +74,7 @@ define(
             {type: 'multisafepay_klarna', component: baseRenderer},
             {type: 'multisafepay_maestro', component: gatewayPath + 'creditcard'},
             {type: 'multisafepay_mastercard', component: gatewayPath + 'creditcard'},
+            {type: 'multisafepay_mbway', component: baseRenderer},
             {type: 'multisafepay_mybank', component: gatewayPath + 'mybank'},
             {type: 'multisafepay', component: baseRenderer},
             {type: 'multisafepay_payafter', component: gatewayPath + 'payafter'},
