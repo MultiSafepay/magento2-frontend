93f89dcf24d83e69e4d167745f60f7fbfe674180
Vinod Sowdagar <53818758+vinodsowdagar@users.noreply.github.com>
2025-01-24 16:20:25 +0100

    PLGMAG2V2-815: Add delay for cancel controller to wait for order to cancel (#296)

--- Service/CancelOrder.php
+++ Service/CancelOrder.php
@@ -6,6 +6,7 @@ namespace MultiSafepay\ConnectFrontend\Service;
 
 use Exception;
 use Magento\Sales\Api\Data\OrderInterface;
+use Magento\Sales\Model\OrderFactory;
 use Magento\Sales\Model\Service\OrderService;
 use MultiSafepay\ConnectCore\Factory\SdkFactory;
 use MultiSafepay\ConnectCore\Logger\Logger;
@@ -30,18 +31,26 @@ class CancelOrder
     private $sdkFactory;
 
     /**
+     * @var OrderFactory
+     */
+    private $orderFactory;
+
+    /**
      * @param OrderService $orderService
      * @param Logger $logger
      * @param SdkFactory $sdkFactory
+     * @param OrderFactory $orderFactory
      */
     public function __construct(
         OrderService $orderService,
         Logger $logger,
-        SdkFactory $sdkFactory
+        SdkFactory $sdkFactory,
+        OrderFactory $orderFactory
     ) {
         $this->orderService = $orderService;
         $this->logger = $logger;
         $this->sdkFactory = $sdkFactory;
+        $this->orderFactory = $orderFactory;
     }
 
     /**
@@ -59,10 +68,28 @@ class CancelOrder
             if ($this->isPaymentLink((int)$order->getStoreId(), $orderId)) {
                 $this->orderService->cancel($order->getEntityId());
                 $this->logger->logInfoForOrder($orderId, 'Pretransaction found, order canceled by Cancel controller');
+
+                return;
             }
         } catch (Exception | ClientExceptionInterface $exception) {
             $this->logger->logInfoForOrder($orderId, 'Exception occurred when trying to cancel order');
             $this->logger->logExceptionForOrder($orderId, $exception);
+        }
+
+        // Waiting for a maximum of 15 seconds for the order to be canceled by the notification before exiting
+        for ($count = 0; $count < 5; $count++) {
+            $updatedOrder = $this->orderFactory->create()->loadByIncrementId($orderId);
+
+            if (!$updatedOrder->canCancel()) {
+                break;
+            }
+
+            $this->logger->logInfoForOrder(
+                $orderId,
+                'Order not canceled yet, waiting for 3 seconds'
+            );
+            //phpcs:ignore
+            sleep (3);
         }
     }
 
